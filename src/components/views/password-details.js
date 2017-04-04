2dcd4ec0c1da3fd02d83ec8125a987853242f9ea
Tomasz Goralczyk <devflag@gmail.com>
2017-04-04 12:25:20 -0400

    Refactored components and improved animation

--- src/components/views/password-details.js
+++ src/components/views/password-details.js
@@ -1,76 +1,98 @@
 import React from 'react';
 
-const passItemStyle = {
-	display: 'flex',
-	alignItems: 'center',
-	background: '#282F5B',
-	height: '85px',
-	width: '95%',
-	borderRadius: '0 500px 500px 0',
-	marginLeft: '2.5px',
-	letterSpacing: '1.65px',
-	margin: '0 0 15px 0',
-	position: 'relative',
-	transition: 'transform .5s ease-in-out',
-};
+const LockIcon = () => (
+	<div style={{
+		fontSize: '1.65em',
+		padding: '0 25px',
+		color: '#F1F1F4',
+	}}><i className="fa fa-lock"></i></div>
+);
 
-const passItemActiveStyle = {
-	...passItemStyle,
-	transform: 'translateX(-50%)',
-};
+const ServiceName = ({ serviceName, optionsShown }) => {
+	let styles = {
+		fontSize: '1.15em',
+		color: '#EE1959',
+		fontWeight: '400',
+		marginBottom: '5px',
+		transition: 'all 500ms ease-in-out',
+	};
+	styles = optionsShown ? {...styles, fontSize: '.95em'} : styles;
+	return (
+		<div style={styles}>{serviceName}</div>
+	);
+}
 
-const passIconStyle = {
-	fontSize: '1.65em',
-	padding: '0 25px 0 25px',
-	color: '#F1F1F4',
-};
+const UserName = ({ userName, optionsShown }) => {
+	let styles = {
+		color: '#F1F1F4',
+		fontSize: '.95em',
+		fontWeight: '300',
+		transition: 'all 500ms ease-in-out',
+	};
+	styles = optionsShown ? 
+		{...styles, opacity: '.4', fontSize: '.85em'} : styles;
+	return (
+		<div style={styles}>{userName}</div>
+	);
+}
 
-const passDetailsStyle = {
-	display: 'flex',
-	flexDirection: 'column',
-};
+const ShowPasswordOptions = ({ showOptions }) => (
+	<div style={{
+		fontSize: '1.15em',
+		color: '#F1F1F4',
+		padding: '2.5px 7.5px',
+		cursor: 'pointer',
+	}} onClick={showOptions}>
+		<i className='fa fa-chevron-right' aria-hidden='true'></i>
+	</div>
+);
 
-const passNameStyle = {
-	fontSize: '1.15em',
-	color: '#EE1959',
-	fontWeight: '400',
-	marginBottom: '5px',
-};
-
-const passUserAccStyle = {
-	color: '#F1F1F4',
-	fontSize: '.95em',
-	fontWeight: '300',
-};
-
-const showPassOptsBtn = {
-	position: 'absolute',
-	right: '10px',
-	fontSize: '1.15em',
-	top: '40%',
-	color: '#F1F1F4',
-	padding: '0 0 0 10px',
-	cursor: 'pointer',
-};
-
-export default (props) => {
-	const styles = props.active ? passItemActiveStyle : passItemStyle;
+const PasswordOverview = ({ serviceName, userName, optionsShown }) => {
+	let styles = {
+		display: 'flex',
+		flexDirection: 'column',
+		flexBasis: '72.5%',
+		transition: 'flex-basis 500ms ease-in-out',
+		overflow: 'hidden',
+	};
+	styles = optionsShown ? 
+		{...styles, flexBasis: '47.5%'} : styles;
 	return (
 		<div style={styles}>
-			<div style={passIconStyle}>
-				<i className="fa fa-lock" />
-			</div>
-			<div style={passDetailsStyle}>
-				<div style={passNameStyle}>
-					{props.name}
-				</div>
-				<div style={passUserAccStyle}>
-					{props.login}
-				</div>
-			</div>
-			<div style={showPassOptsBtn} onClick={props.showOptions} >
-				<i className="fa fa-chevron-right" aria-hidden="true"></i>
-			</div>
+			<ServiceName 
+				serviceName={serviceName} 
+				optionsShown={optionsShown} />
+			<UserName 
+				userName={userName} 
+				optionsShown={optionsShown} />
 		</div>
 	);
 };
+
+const PasswordDetails = (props) => {
+	let styles = {
+		display: 'flex',
+		alignItems: 'center',
+		background: '#282F5B',
+		height: '85px',
+		width: '95%',
+		borderRadius: '0 500px 500px 0',
+		marginLeft: '2.5px',
+		letterSpacing: '1.65px',
+		position: 'relative',
+		transition: 'width 500ms ease-in-out',
+	};
+	styles = props.active ? {...styles, width: '50%'} : styles;
+	return (
+		<div style={styles}>
+			<LockIcon />
+			<PasswordOverview
+				serviceName={props.name}
+				userName={props.login}
+				optionsShown={props.active} />
+			<ShowPasswordOptions showOptions={props.showOptions} />
+		</div>
+	);
+}
+
+export default PasswordDetails;
