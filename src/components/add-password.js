7ee4b9e30dff6a7eaef7cca3efedc0c967184ff2
Tomasz Goralczyk <devflag@gmail.com>
2017-04-06 09:12:26 -0400

    Created a component that uses sliders to show a receipt for generating new passwords

--- src/components/add-password.js
+++ src/components/add-password.js
@@ -2,6 +2,63 @@ import React, { Component } from 'react';
 import * as _ from 'lodash';
 import './add-password.css';
 
+const passwordGenerator = {
+	specialCharacters: [
+		']', '[', '?', '/',
+		'<', '~', '#', '`',
+		'!', '@', '$', '%', 
+		'^', '&', '*', '(',
+		')', '+', '=', '}',
+		'|', ':', '"', ';',
+		'_', '.', '>', '{'
+	],
+
+	letters: [
+		'a', 'b', 'c', 'd',
+		'e', 'f', 'g', 'h',
+		'i', 'j', 'k', 'l',
+		'm', 'n', 'o', 'p',
+		'r', 's', 't', 'u',
+		'w', 'z'
+	],
+
+	getRandomCharacter(characterSet) {
+		const min = 0;
+		const max = characterSet.length - 1;
+		const position = Math.floor(Math.random() * (max - min)) + min;
+
+		return characterSet[position];
+	},
+
+	generate(length, numOfNumbers = 0, numOfSpecChars = 0) {
+		let password = '';
+		let tempArray = [];
+		const numOfLetters = (length - numOfNumbers) - numOfSpecChars;
+
+		for (let i = 0; i < numOfLetters; i++) {
+			let randomLetter = this.getRandomCharacter(this.letters);
+			randomLetter = (i % 3 === 0) ? randomLetter.toUpperCase() : randomLetter;
+			tempArray.push(randomLetter);
+		}
+
+		for (let i = 0; i < numOfNumbers; i++) {
+			const randomNumber = Math.floor(Math.random() * 10);
+			tempArray.push(randomNumber.toString());
+		}
+
+		for (let i = 0; i < numOfSpecChars; i++) {
+			tempArray.push(this.getRandomCharacter(this.specialCharacters));
+		}
+
+		tempArray = tempArray.slice(0, length);
+		tempArray = _.shuffle(tempArray);
+		password = tempArray.join(' ').replace(/\s+/g, '');
+
+		return password;
+	}
+
+}
+
 const styles = {
 	background: '#2D3665',
 	width: '85%',
@@ -22,66 +79,6 @@ class AddPasswordForm extends Component {
 
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
-		this.generateNewPassword = this.generateNewPassword.bind(this);
-	}
-
-	generateNewPassword() {
-		const specialCharacters = [
-			']', '[', '?', '/',
-			'<', '~', '#', '`',
-			'!', '@', '$', '%', 
-			'^', '&', '*', '(',
-			')', '+', '=', '}',
-			'|', ':', '"', ';',
-			'_', '.', '>', '{'
-		];
-		const letters = [
-			'a', 'b', 'c', 'd',
-			'e', 'f', 'g', 'h',
-			'i', 'j', 'k', 'l',
-			'm', 'n', 'o', 'p',
-			'r', 's', 't', 'u',
-			'w', 'z'
-		];
-
-		const getRandomCharacter = (characterSet) => {
-			const min = 0;
-			const max = characterSet.length - 1;
-			const position = Math.floor(Math.random() * (max - min)) + min;
-
-			return characterSet[position];
-		};
-
-		const generatePassword = 
-			(length, numOfNumbers = 0, numOfSpecChars = 0) => {
-				let password = '';
-				let tempArray = [];
-				const numOfLetters = (length - numOfNumbers) - numOfSpecChars;
-
-				for (let i = 0; i < numOfLetters; i++) {
-					let randomLetter = getRandomCharacter(letters);
-					randomLetter = (i % 3 === 0) ? randomLetter.toUpperCase() : randomLetter;
-					tempArray.push(randomLetter);
-				}
-
-				for (let i = 0; i < numOfNumbers; i++) {
-					const randomNumber = Math.floor(Math.random() * 10);
-					tempArray.push(randomNumber.toString());
-				}
-
-				for (let i = 0; i < numOfSpecChars; i++) {
-					tempArray.push(getRandomCharacter(specialCharacters));
-				}
-
-				tempArray = _.shuffle(tempArray);
-				password = tempArray.join(' ').replace(/\s+/g, '');
-
-				return password;
-			}
-
-		console.log(generatePassword(25));
-		console.log(generatePassword(20, 8, 5));
-		console.log(generatePassword(50, 12, 12));
 	}
 
 	handleChange(event) {
@@ -138,9 +135,97 @@ class AddPasswordForm extends Component {
 						<input type="submit" value="Create" />
 					</div>
 				</form>
+				<GenerateNewPassword />
+
 			</div>
 		);
 	}
 }
+
+class GenerateNewPassword extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			length: 15,
+			digits: 3,
+			symbols: 2,
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.generatePassword = this.generatePassword.bind(this);
+	}
+
+	componentWillMount() {
+		// Generate a new password
+		this.generatePassword(this.state);
+	}
+
+	handleChange(event) {
+		const target = event.target;
+		const value = +target.value;
+		const name = target.name;
+
+		this.setState((prevState, currProps) => {
+			const newState = {...prevState, [name]: value};
+			this.generatePassword(newState);
+			return newState;
+		});
+	}
+
+	generatePassword(receipt) {
+		let password = '';
+		const { length, digits, symbols } = receipt;
+
+		if (digits >= length) {
+			password = passwordGenerator.generate(length, length, 0);
+		} else if (digits === 0 && symbols >= length) {
+			password = passwordGenerator.generate(length, 0, length);
+		} else {
+			password = passwordGenerator.generate(length, digits, symbols);
+		}
+
+		console.log(password);
+	}
+
+	render() {
+		return (
+			<div className="passwordGenerator">
+				<div className="slider">
+					<div className="name">Length</div>
+					<input 
+						type="range" 
+						min="0" 
+						max="64"
+						name="length"
+						value={this.state.length}
+						onChange={this.handleChange} />
+					<div className="count">{this.state.length}</div>
+				</div>
+				<div className="slider">
+					<div className="name">Digits</div>
+					<input 
+						type="range" 
+						min="0" 
+						max="10" 
+						name="digits"
+						value={this.state.digits}
+						onChange={this.handleChange} />
+					<div className="count">{this.state.digits}</div>
+				</div>
+				<div className="slider">
+					<div className="name">Symbols</div>
+					<input 
+						type="range" 
+						min="0" 
+						max="10"
+						name="symbols"
+						value={this.state.symbols}
+						onChange={this.handleChange} />
+					<div className="count">{this.state.symbols}</div>
+				</div>
+			</div>
+		);
+	}
+}
+
 
 export default AddPasswordForm;
