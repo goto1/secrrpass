5b869a7b15aa9d94ada17554e41282303774cea8
Tomasz Goralczyk <devflag@gmail.com>
2017-04-16 07:07:15 -0400

    Minor changes

--- src/utils/id-generator.js
+++ src/utils/id-generator.js
@@ -1,29 +1,29 @@
 import * as _ from 'lodash';
 import randGenerator from './random-generator';
 
-const { letters, getRandomCharFrom, genRandomNum } = randGenerator;
+const { letters, getRandomCharFrom, generateRandomNum } = randGenerator;
 
-const genRandomID = () => {
+const generateRandomID = () => {
+	let randomID = [];
 	const length = 6;
 	const middle = Math.floor(length / 2);
-	let randomID = [];
 
 	for (let i = 0; i < middle; i++) {
 		randomID.push(getRandomCharFrom(letters));
 	}
 
 	while (randomID.length < length) {
-		const generatedNum = genRandomNum();
+		const generatedNum = generateRandomNum();
 		const duplicate = randomID.find(num => num === generatedNum);
+
 		if (duplicate === undefined) {
 			randomID.push(generatedNum);
 		}
 	}
 
-	randomID = _.shuffle(randomID);
-	randomID = randomID.join(' ').replace(/\s+/g, '');
+	randomID = _.shuffle(randomID).join(' ').replace(/\s+/g, '');
 
 	return randomID;
 };
 
-export default genRandomID;
+export default generateRandomID;
