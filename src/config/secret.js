3022d601cd4670b01447ff0753c55e2c6beab356
Tomasz Goralczyk <devflag@gmail.com>
2017-04-20 09:29:12 -0400

    Config file

---
+++ src/config/secret.js
@@ -0,0 +1,4 @@
+export default {
+	key: '3bP:iciPglHljrTnGsSaww8TmHgtsddiuJBrRgmdLb8',
+	firebase: '{"iv":"IY1tNHwuk2uhG46Ecne3RQ==","v":1,"iter":10000,"ks":128,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"lSE1jsdWfUk=","ct":"va3DPJq79RurC2FDl0zfKnsQLzel7s28WpMzlIx0NyH/a+0dS1UI0IUZq4+T9fdv1yyhjZXVQWrrgra0sw0XtBpHuAQ0kK+y10UVNMt7hwilrkhUkDTyEeUzsMTXuG8kUzDjoeIXfg5d4E7ddkCKsaeNxm0SKYXT3Xmnx68tvJqW6o+D/B6fezCT5tMDONnn5aNYQw8uZJ5lTo9sp9mrS0ddMH4zrX9KXBxQET/vuwluQ/YQRV/2Ai4RRuq5pf5XIvyNYwaFfQPc62mTR1hegMkhd91kTW3NylVQAGwNcfpY9WbGHDZNG2982FyW9zHErM1PgUx3fV+x"}',
+};
