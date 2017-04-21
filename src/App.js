416955ec07525c676e992e6c1530ba419669bda2
Tomasz Goralczyk <devflag@gmail.com>
2017-04-21 07:28:53 -0400

    Added the logic to create a new user in the database if already not existent

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
+import genUserID from './utils/id-generator';
+import firebase from './utils/firebase';
+import MainLayout from './components/layouts/main-layout';
 import './App.css';
 
-import MainLayout from './components/layouts/main-layout';
-
 class App extends Component {
-  render() {
-    return (
-      <MainLayout />
-    );
-  }
+
+	componentWillMount() {
+		const { match, history } = this.props;
+		const userID = match.params.userID || genUserID();
+
+		firebase.checkIfUserExists(userID)
+			.then((user) => {
+				if (user.val() === null) { 
+					firebase.createNewUser(userID); 
+				} else {
+					firebase.updateUserLastAccess(userID);
+				}
+				localStorage.setItem('userID', userID);
+				history.push(`/${userID}`);
+			})
+			.catch((err) => { console.log(err); });
+	}
+
+	render() {
+		return <MainLayout />
+	}
 }
 
 export default App;
