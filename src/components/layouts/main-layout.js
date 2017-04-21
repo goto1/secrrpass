220dbf6129757901cb69800cf606657935139c39
Tomasz Goralczyk <devflag@gmail.com>
2017-04-21 07:26:44 -0400

    Minor refactoring

--- src/components/layouts/main-layout.js
+++ src/components/layouts/main-layout.js
@@ -1,32 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Header from '../containers/header';
 import PasswordList from '../containers/password-list';
 import AddPassword from '../containers/add-password';
 import Settings from '../settings';
 
-const layoutStyle = {
-	background: '#1C1F41',
-	width: '375px',
-	height: '667px',
-	borderRadius: '5px',
-	boxShadow: '0 0 20px 5px #323332',
-	overflow: 'hidden',
-	display: 'flex',
-	flexDirection: 'column',
+const styles = {
+	layout: {
+		background: '#1C1F41',
+		width: '375px',
+		height: '667px',
+		borderRadius: '5px',
+		boxShadow: '0 0 20px 5px #323332',
+		overflow: 'hidden',
+		display: 'flex',
+		flexDirection: 'column',
+	},
+	content: {
+		overflowY: 'scroll',
+		paddingTop: '5px',
+		height: '100%',
+	},
 };
 
 const MainLayout = () => (
-	<div style={layoutStyle}>
+	<div style={styles.layout}>
 		<Header />
-		<div style={{
-			overflowY: 'scroll',
-			paddingTop: '5px',
-			height: '100%',
-		}}>
-			<Route exact path="/:userID?" component={PasswordList} />
-			<Route path="/:userID?/add" component={AddPassword} />
-			<Route path="/:userID?/settings" component={Settings} />
+		<div style={styles.content}>
+			<Route path="/:userID?" component={PasswordList} />
+			<Route path="/:userID/add" component={AddPassword} />
+			<Route path="/:userID/settings" component={Settings} />
 		</div>
 	</div>
 );
