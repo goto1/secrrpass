e01e43d54db9a7701bce1db55d679ba456e57c0c
Tomasz Goralczyk <devflag@gmail.com>
2017-04-22 12:08:44 -0400

    Change the implementation for checking if user exists from promises to observables

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import genUserID from './utils/id-generator';
 import firebase from './utils/firebase';
 import MainLayout from './components/layouts/main-layout';
 import './App.css';
+import 'rxjs/add/operator/map';
 
 class App extends Component {
 
@@ -10,17 +11,34 @@ class App extends Component {
 		const { match, history } = this.props;
 		const userID = match.params.userID || genUserID();
 
+		const extractData = (data) => data.val() || null;
+
 		firebase.checkIfUserExists(userID)
-			.then((user) => {
-				if (user.val() === null) { 
-					firebase.createNewUser(userID); 
-				} else {
-					firebase.updateUserLastAccess(userID);
-				}
-				localStorage.setItem('userID', userID);
-				history.push(`/${userID}`);
-			})
-			.catch((err) => { console.log(err); });
+			.map(extractData)
+			.subscribe(
+				(user) => {
+					if (!user) {
+						firebase.createNewUser(userID);
+					} else {
+						firebase.updateUserLastAccess(userID);
+					}
+					localStorage.setItem('userID', userID);
+					history.push(`/${userID}`);
+				},
+				(err) => { console.log(err); },
+			);
+
+		// firebase.checkIfUserExists(userID)
+		// 	.then((user) => {
+		// 		if (user.val() === null) { 
+		// 			firebase.createNewUser(userID); 
+		// 		} else {
+		// 			firebase.updateUserLastAccess(userID);
+		// 		}
+		// 		localStorage.setItem('userID', userID);
+		// 		history.push(`/${userID}`);
+		// 	})
+		// 	.catch((err) => { console.log(err); });
 	}
 
 	render() {
