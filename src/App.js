595a04b117aceabe543fb6a82cb7513e38b94ab4
Tomasz Goralczyk <devflag@gmail.com>
2017-04-23 11:17:20 -0400

    Minor refactoring, moved the logic to the PasswordList component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,14 @@
-import React, { Component } from 'react';
-import genUserID from './utils/id-generator';
-import firebase from './utils/firebase';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import MainLayout from './components/layouts/main-layout';
 import './App.css';
-import 'rxjs/add/operator/map';
 
-class App extends Component {
+import './utils/rxjs-operators';
 
-	componentWillMount() {
-		const { match, history } = this.props;
-		const userID = match.params.userID || genUserID();
-
-		const extractData = (data) => data.val() || null;
-
-		firebase.checkIfUserExists(userID)
-			.map(extractData)
-			.subscribe(
-				(user) => {
-					if (!user) {
-						firebase.createNewUser(userID);
-					} else {
-						firebase.updateUserLastAccess(userID);
-					}
-					localStorage.setItem('userID', userID);
-					history.push(`/${userID}`);
-				},
-				(err) => { console.log(err); },
-			);
-
-		// firebase.checkIfUserExists(userID)
-		// 	.then((user) => {
-		// 		if (user.val() === null) { 
-		// 			firebase.createNewUser(userID); 
-		// 		} else {
-		// 			firebase.updateUserLastAccess(userID);
-		// 		}
-		// 		localStorage.setItem('userID', userID);
-		// 		history.push(`/${userID}`);
-		// 	})
-		// 	.catch((err) => { console.log(err); });
-	}
-
-	render() {
-		return <MainLayout />
-	}
-}
+const App = () => (
+	<Router>
+		<MainLayout />
+	</Router>
+);
 
 export default App;
