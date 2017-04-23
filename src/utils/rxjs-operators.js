f9d049d949c8516027098f6df93d5e9bacc5cc52
Tomasz Goralczyk <devflag@gmail.com>
2017-04-23 11:45:34 -0400

    Exported the rxjs operators to a separate file

---
+++ src/utils/rxjs-operators.js
@@ -0,0 +1,6 @@
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/fromPromise';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/debounceTime';
