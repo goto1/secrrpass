a2412d570494c7e3c6521ab20e66533e8f2b4e16
Tomasz Goralczyk <devflag@gmail.com>
2017-05-12 12:18:09 -0400

    Extracted a function that handles changes in form fields

--- src/utils/form.js
+++ src/utils/form.js
@@ -1,17 +1,47 @@
 import * as _ from 'lodash';
 
-const checkIfFormValid = (formFields) => {
-	const isValid = _.reduce(formFields, 
+function formField(attributes, description) {
+	return {
+		attr: { value: '', ...attributes },
+		valid: false,
+		touched: false,
+		desc: description,
+	};
+}
+
+function updateFormField(event, formFields) {
+	if (!event || !formFields) { return {}; }
+
+	const { name, value } = event.target;
+	const formField = Object.assign({}, formFields[name]);
+
+	const updated = {
+		...formFields,
+		[name]: {
+			...formField,
+			attr: { ...formField.attr, value },
+			valid: value.length > 0 ? true : false,
+			touched: true,
+		},
+	};
+
+	return updated;
+}
+
+function checkIfValidForm(formFields) {
+	const isValid = _.reduce(formFields,
 		(result, value) => result && value.valid, true);
 
 	return isValid;
+}
+
+function checkIfMatchingFields(field1, field2) {
+	return field1.value === field2.value;
+}
+
+export {
+	formField, 
+	updateFormField, 
+	checkIfValidForm, 
+	checkIfMatchingFields 
 };
-
-const checkIfFieldsMatching = (field1, field2) => field1.value === field2.value;
-
-const formUtils = {
-	checkIfFormValid,
-	checkIfFieldsMatching,
-};
-
-export default formUtils;
