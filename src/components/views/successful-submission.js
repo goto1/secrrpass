0c14e336618f3f39558e98c715bd0e1c4c6151ce
Tomasz Goralczyk <devflag@gmail.com>
2017-05-15 13:34:26 -0400

    Put the successful submission component into a separate file for better reusability

---
+++ src/components/views/successful-submission.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { genDefaultBtn } from '../../utils/form';
+
+function SuccessfulSubmission({ message, actionName, action }) {
+	const styles = {
+		container: {
+			textAlign: 'center',
+			marginTop: '25%',
+			padding: '20px',
+			fontWeight: '200',
+			letterSpacing: '2px',
+			lineHeight: '35px',
+		},
+		message: {
+			margin: '30px 0',
+			fontSize: '22.5px',
+			textTransform: 'uppercase',
+			color: '#F3F4F5'
+		},
+		button: {
+			display: 'flex',
+			justifyContent: 'space-between',
+			margin: '25px auto',
+		},
+	};
+	const button = genDefaultBtn(actionName, action);
+
+	return (
+		<div style={styles.container}>
+			<div style={styles.message}>
+				<div>{message}</div>
+			</div>
+			<div style={styles.message}>
+				{ button }
+			</div>
+		</div>
+	);
+}
+
+export default SuccessfulSubmission;
+
+// const success = 
+// 	<SuccessfulSubmission
+// 		text='Your password was created!'
+// 		actionName='Add another password'
+// 		action={showForm} />;
+
+// const success = 
+// 	<SuccessfulSubmission
+// 		text='Your password was successfully updated!'
+// 		actionName='Home'
+// 		action={goHome} />;
