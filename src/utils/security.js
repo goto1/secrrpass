161dc232a9cd8052f186846ea5ae3bb06ba3fbec
Tomasz Goralczyk <devflag@gmail.com>
2017-05-21 17:00:00 -0400

    Added checks to eliminate potential bugs

--- src/utils/security.js
+++ src/utils/security.js
@@ -3,22 +3,34 @@ import sjcl from 'sjcl';
 import secret from '../config/secret';
 
 function encrypt(data) {
-	const json = JSON.stringify(data);
+	if (!data) { return null; }
 
-	return sjcl.encrypt(secret.key, json);
+	let temp = data;
+
+	if (typeof data === 'object') {
+		temp = JSON.strinigfy({ ...data });
+	}
+
+	return sjcl.encrypt(secret.key, temp);
 }
 
 function decrypt(data) {
+	if (!data) { return null; }
+
 	const decrypted = sjcl.decrypt(secret.key, data);
 
 	return JSON.parse(decrypted);
 }
 
 function generatePasswordHash(password) {
+	if (!password) { return null; }
+
 	return bcrypt.hashSync(password, 10);
 }
 
 function compareHashToPassword(password, hash) {
+	if (!password || !hash) { return null; }
+	
 	return bcrypt.compareSync(password, hash);
 }
 
