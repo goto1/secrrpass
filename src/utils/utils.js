a716fbf2f20c70da94fec2bd6d1667967a6df444
Tomasz Goralczyk <devflag@gmail.com>
2017-05-26 13:30:24 -0400

    Added more utility functions for exporting

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,9 +1,15 @@
 import API from './api';
 import ErrorHandler from './error-handler';
 import UserUtils from './user';
+import FormUtils from './form';
+import ResponseHandler from './response-handler';
+import SecurityUtils from './security';
 
 export {
 	API,
 	ErrorHandler,
+	ResponseHandler,
 	UserUtils,
+	FormUtils,
+	SecurityUtils,
 };
